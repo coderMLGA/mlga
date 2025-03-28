471c905cf0621d5cc2f44c06551992d062a28a09
coderMLGA <cybercoderprod@gmail.com>
2025-03-29 02:18:14 +0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,8 @@ import { StreamList } from "~/components/streamList";
 export default function Home() {
   return (
     <section>
+<div class="mt-5 text-center text-2xl font-bold text-red-500">В разработке</div>
+<div class="text-center font-bold text-red-500">Открытие <span class="animate-pulse rounded bg-red-500 px-3 py-1 text-white">25 числа</span></div>
       <StreamList />
     </section>
   );
